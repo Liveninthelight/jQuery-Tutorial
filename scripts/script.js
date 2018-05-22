70eb2e4839ab6ad8180a3eb87cc4edc4fe56efb9
Joe Keenan <JoeKeenan22@me.com>
2018-05-22 04:40:47 -0400

    Part20

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,16 @@
 
 
 $(document).ready(function(){
-    
-    function complete() {
-        alert('Animation Complete');
-    }
-    $("section > h2").on("click", function(event){
-        $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "swing", complete);
-    });                                                                //linear
-});
+
+    $("section > h2").on("click", function(){ 
+        $(this).fadeTo(200, 0.2)
+               .fadeTo(200, 0.8)
+               .fadeTo(200, 0.2)
+               .fadeTo(200, 0.8)
+               .fadeTo(200, 0.2)
+               .fadeTo(200, 0.8);
+        $(this).fadeOut().fadeIn();
+        $(this).animate({"opacity" : "0.5"});
+     });
+ });
+ 
