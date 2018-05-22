f6f2b1d9a00d37800caad711330d47ddf042c812
Joe Keenan <JoeKeenan22@me.com>
2018-05-22 03:11:35 -0400

    Part10

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,26 @@
 /*global $*/
-// SOME CODE TO PULL IN MY LATEST TWEET //
-var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'>The big fight live: Ham vs Cheese!</div>";
 
-$("#tweets div p").after(tweet);
-$("#tweets div").html(tweet);
-$("#tweets div p").text(tweet);
 
-// ADDING & CHANGING CONTENT
+// $("section").wrap("<div>");
+// $("section").unwrap();
+// $("section").wrapAll("<div>"); 
 
-/* 
-.append() adds content to bottom of the element
-.prepend() adds content to top of element
-.before() adds content before element
-.after() adds content after element
-.html() changes the whole html of the element
-.text() changes the text of an element 
-*/
+// wrap() - wraps all matched elements individually
+// unwrap() - unwraps all matched elements
+// wrapALl() - wraps all elements combined with 1 single element
+
+var wrapper = "<div class='wrapper'>";
+var button = $(".button");
+var wrapped = true;
+
+button[0].onclick = function(){
+    if (wrapped) {
+        $("section").unwrap();
+        wrapped = false;
+        button.text("Wrap");
+    } else {
+        $("section").wrapAll(wrapper);
+        wrapped = true;
+        button.text("Unwrap");
+    }
+}; 
