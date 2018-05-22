0e1cf0f5bd0dc9229efc6b8d80e747ad2cd43e0d
Joe Keenan <JoeKeenan22@me.com>
2018-05-22 05:10:52 -0400

    Part24

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,10 @@
 /*global $*/
-
-
-
 $(document).ready(function(){
     
-    var allQuotes = $("blockquote");
-    var currentQuote = 0;
-   
-    function changeQuote() {
-        
-        $(allQuotes[currentQuote]).fadeOut(200, function(){
-            
-            if(currentQuote == allQuotes.length - 1){
-                currentQuote = 0;
-            } else {
-                currentQuote++;
-            }
-        
-            $(allQuotes[currentQuote]).fadeIn(200);
-        });
+    var items = $("#points-of-sale li");
+    
+    items.on('click', function(){
+       $(this).find("p").slideToggle(500); 
+    });
 
-    }
-    
-    var quoteTimer = setInterval(changeQuote, 3000);
-    
-});    
+}); 
