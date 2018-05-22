5fe13969cff7754c64e12b8613a4c8e2b923607b
Joe Keenan <JoeKeenan22@me.com>
2018-05-22 03:05:10 -0400

    Part9

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,18 @@
-//Not best practice 
-$("#contact-methods").css({border: "2px solid red"}).next().css({border:"2px solid green"}).closest("section").css({border: "2px solid blue"});
+/*global $*/
+// SOME CODE TO PULL IN MY LATEST TWEET //
+var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'>The big fight live: Ham vs Cheese!</div>";
 
-//Best Practice 
-$("#contact-methods").css({border: "2px solid red"})
-    .next().css({border: "2px solid green"})
-    .closest("section").css({border: "2px solid blue"}); 
+$("#tweets div p").after(tweet);
+$("#tweets div").html(tweet);
+$("#tweets div p").text(tweet);
+
+// ADDING & CHANGING CONTENT
+
+/* 
+.append() adds content to bottom of the element
+.prepend() adds content to top of element
+.before() adds content before element
+.after() adds content after element
+.html() changes the whole html of the element
+.text() changes the text of an element 
+*/
