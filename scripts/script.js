d18d0e9050f25723f504f13cdef8e31b21b6c2ad
Joe Keenan <JoeKeenan22@me.com>
2018-05-22 03:57:12 -0400

    Part17

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,8 @@
 /*global $*/
 
-    
-$("#lead-banner").click(function(){
-    alert("You double clicked me");
-});
 
-$("#lead-banner").on("dblclick", function(){
-    alert("You double clicked me");
-});
-
-$("#lead-banner").dblclick( function(){
-    alert("You double clicked me");
-    $("#lead-banner").off("dblclick");
-});
-
-$("#lead-banner").dblclick(function(){
-    alert('you double clicked me');
-    $("#lead-banner").off("dblclick");
-});
+$(document).ready(function() {});
+$(window).on("load", function() {});
 
 
-
-
-
-
-// on() - binds an event to matched element(s)
-// off() - unbinds event from matched element(s)
-
-
-
-
-var button = $("#lead-banner a");
- 
-button[0].onclick = function() {
-    $("#points-of-sale").toggleClass("open");
-    return false;
-}
 
