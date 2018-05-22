c3a1bb5eab3353880e1cc01c86f6decf104c9492
Joe Keenan <JoeKeenan22@me.com>
2018-05-22 02:10:35 -0400

    Part4

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,23 @@ $(document).ready(function() {
 
 $("img[alt=quote]").css({border: "2px solid purple"});
 
+/* Console Example Below
+
+>  document.getElementById("page-title");
+<     <h2 id="page-title">Kung Fu Your Net Skills!</h2>
+
+> $("#page-title");
+< [   <h2 id="page-title">Kung Fu Your Net Skilld!</h2>]
+
+> var heading = $("page-title");
+< undefined 
+
+> heading.css({position: "relative});
+< [  <h2 id="page-title">Kung Fu Your Net Skills!</h2>]
+
+> heading.animate({left: 100});
+< [  <h2 id="page-title" style="position: relative; left: < 100px;">Kung Fu Your Net Skills!</h2>]
+
+> heading[0].animate({left:20});
+< Animation {}
+> 
