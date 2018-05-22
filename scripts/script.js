87520c3297c0e73013ea5d9fe323563e8e4e8370
Joe Keenan <JoeKeenan22@me.com>
2018-05-22 04:31:20 -0400

    Part18

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
 /*global $*/
 
 
-$(document).ready(function() {});
-$(window).on("load", function() {});
-
-
-
+$(document).ready(function(){
+    $("*").on("click", function(e){
+        console.log(e.target);
+        console.log("the event type is: " + e.type);
+        console.log("x-coordinate of the event is: " + e.pageX);
+        console.log("y-coordinate of the event is: " + e.pageY);
+        e.stopPropagation(); 
+    });
+});
